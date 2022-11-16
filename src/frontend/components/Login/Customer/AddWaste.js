f86346048eeb00bb81e35b78c6d994d7685e2b23
Taha_Firoz <tahafbharucha@gmail.com>
2022-11-16 13:50:07 +0530

    Add Waste Component

--- src/frontend/components/Login/Customer/AddWaste.js
+++ src/frontend/components/Login/Customer/AddWaste.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+import './AddWaste.css';
 
 const AddWaste = () => {
-  return (
-    <div>AddWaste</div>
-  )
-}
+  const [wasteList, setWasteList] = useState([]);
+  const [waste, setWaste] = useState('Select Waste');
+  const NewWaste = () => {
+    console.log('called');
+    return (
+      <div>
+        <select waste={waste}>
+          <option waste='plastic'>Plastic Waste</option>
+          <option waste='steel'>Steel Waste</option>
+          <option waste='biodegradable'>Biodegradable Waste</option>
+          <option waste='ewaste'>E-Waste</option>
+        </select>x
+      </div>
+    );
+  };
 
-export default AddWaste
+  const addWaste = (e) => {
+    setWasteList(wasteList.concat(<NewWaste />));
+  };
+  return (
+    <div>
+      <input placeholder=''/>
+    </div>
+  );
+};
+
+export default AddWaste;
