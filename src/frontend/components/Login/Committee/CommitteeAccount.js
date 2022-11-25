70ad9801af68b65016071c116773572ada28f1e3
Taha_Firoz <tahafbharucha@gmail.com>
2022-11-25 17:53:44 +0530

    Committee Account

--- src/frontend/components/Login/Committee/CommitteeAccount.js
+++ src/frontend/components/Login/Committee/CommitteeAccount.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import './CommitteeAccountStyle.css';
 
 const CommitteeAccount = () => {
+  const mockData = {
+    name: 'Rahul Shah',
+    id: '12',
+    contactNumber: '+91 7548639585',
+  };
   return (
-    <div>CommitteeAccount</div>
-  )
-}
+    <div className='userBox'>
+      <div className='innerBox'>
+        <div className='row'>
+          <div className='cell'>Name : </div>
+          <div className='cell'>{mockData.name}</div>
+        </div>
+        <div className='row'>
+          <div className='cell'>Id : </div>
+          <div className='cell'>{mockData.id}</div>
+        </div>
+        <div className='row'>
+          <div className='cell'>Contact Number : </div>
+          <div className='cell'>{mockData.contactNumber}</div>
+        </div>
+        <button className='editButton'>Edit Details</button>
+      </div>
+    </div>
+  );
+};
 
-export default CommitteeAccount
+export default CommitteeAccount;
