c46cbabbe044e365c19f195cb5f945ba5d1102c5
Taha_Firoz <tahafbharucha@gmail.com>
2022-12-06 17:53:16 +0530

    Customer Account

--- src/frontend/components/Login/Customer/CustomerAccount.js
+++ src/frontend/components/Login/Customer/CustomerAccount.js
@@ -1,13 +1,29 @@
-import React from 'react'
-import './CustomerAccountStyle.css'
+import React from 'react';
+import './CustomerAccountStyle.css';
 const CustomerAccount = () => {
-  const cust = {
-    name:'Taha Firoz',
-    
-  }
+  const mockData = {
+    name: 'Taha Firoz',
+    id: '12',
+    address: 'This is the address',
+  };
   return (
-    <div className='customerBox'>CustomerAccount</div>
-  )
-}
+    <div className='customerBox'>
+      <div className='innerBox'>
+        <div className='row'>
+          <div className='cell'>Name : </div>
+          <div className='cell'>{mockData.name}</div>
+        </div>
+        <div className='row'>
+          <div className='cell'>Id : </div>
+          <div className='cell'>{mockData.id}</div>
+        </div>
+        <div className='row'>
+          <div className='cell'>Address : </div>
+          <div className='cell'>{mockData.address}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default CustomerAccount
+export default CustomerAccount;
